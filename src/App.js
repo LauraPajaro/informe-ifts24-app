bf6fa5971e0144615bbafb7b19835b2343337f8b
laurapajaro <lauratecno30@gmail.com>
2024-06-08 14:28:23 -0300

    Informe embebido

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <iframe title="TPGRUPAL" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiOWE4MjdlYTQtNGIzZS00MTcwLTk3YzktYmUwNGRkYjAwYTlkIiwidCI6IjYxZTlhYWViLWFmOWMtNDNmYi1iMjIxLTgwNGU3ZTQ0N2JhNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
     </div>
   );
 }
