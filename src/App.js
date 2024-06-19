3e8b814a923c2d5aa84daf2aaf67738b379a0901
laurapajaro <lauratecno30@gmail.com>
2024-06-19 12:47:40 -0300

    Datos y creadores

--- src/App.js
+++ src/App.js
@@ -4,23 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <iframe title="TPGRUPAL" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiOWE4MjdlYTQtNGIzZS00MTcwLTk3YzktYmUwNGRkYjAwYTlkIiwidCI6IjYxZTlhYWViLWFmOWMtNDNmYi1iMjIxLTgwNGU3ZTQ0N2JhNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
-    </div>
-  );
+   <p>REPORTE LAU</p>
+   <iframe title="TPGRUPAL" width="800" height="590" src="https://app.powerbi.com/view?r=eyJrIjoiOWE4MjdlYTQtNGIzZS00MTcwLTk3YzktYmUwNGRkYjAwYTlkIiwidCI6IjYxZTlhYWViLWFmOWMtNDNmYi1iMjIxLTgwNGU3ZTQ0N2JhNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+   <p>REPORTE GER</p>
+   <iframe title="ReportGer" width="800" height="590" src="https://app.powerbi.com/view?r=eyJrIjoiZTc4MmViNzUtZGRlZC00ZDc4LTg5NmEtYmMzMjY5NzIzZjAyIiwidCI6IjYxZTlhYWViLWFmOWMtNDNmYi1iMjIxLTgwNGU3ZTQ0N2JhNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+
+   </div> );
 }
 
 export default App;
