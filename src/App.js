803256f445d66009a351bfb006f3e5c6f4748fff
laurapajaro <lauratecno30@gmail.com>
2024-06-20 17:56:09 -0300

    Reporte

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
    <p>REPORTE LAU</p>
-   <iframe title="TPGRUPAL" width="800" height="590" src="https://app.powerbi.com/view?r=eyJrIjoiOWE4MjdlYTQtNGIzZS00MTcwLTk3YzktYmUwNGRkYjAwYTlkIiwidCI6IjYxZTlhYWViLWFmOWMtNDNmYi1iMjIxLTgwNGU3ZTQ0N2JhNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+   <iframe title="TPGRUPAL" width="800" height="590" src="https://app.powerbi.com/view?r=eyJrIjoiOWE4MjdlYTQtNGIzZS00MTcwLTk3YzktYmUwNGRkYjAwYTlkIiwidCI6IjYxZTlhYWViLWFmOWMtNDNmYi1iMjIxLTgwNGU3ZTQ0N2JhNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>      <p>REPORTE GER</p>
    <p>REPORTE GER</p>
    <iframe title="ReportGer" width="800" height="590" src="https://app.powerbi.com/view?r=eyJrIjoiZTc4MmViNzUtZGRlZC00ZDc4LTg5NmEtYmMzMjY5NzIzZjAyIiwidCI6IjYxZTlhYWViLWFmOWMtNDNmYi1iMjIxLTgwNGU3ZTQ0N2JhNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
 
